c5258c4464d40d06ed94240d8fa4932a33eceaf5
pleo2 <leooel23m@gmail.com>
2025-05-31 18:30:56 -0400

    chore(deps): update dependencies and improve Swagger configuration
    - Se agregó la regla "no-unused-vars" en .eslintrc para advertencias.
    - Se actualizaron las dependencias de NestJS a versiones más recientes.
    - Se añadió Fastify como framework web y se configuró Swagger para la documentación de la API.
    - Se implementó validación global con ValidationPipe en el bootstrap de la aplicación.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { Logger } from "@nestjs/common";
+import "reflect-metadata";
+import { Logger, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "@/app/app.module";
+const GLOBAL_PREFIX = "api";
+const SWAGGER_PATH = "doc";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -14,14 +18,39 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
-  app.setGlobalPrefix("api");
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  // Habilita la validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle("nestjs-template")
+    .setDescription("nestjs-template API description")
+    .setVersion("1.0")
+    .addTag("nestjs-template")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`/${GLOBAL_PREFIX}/${SWAGGER_PATH}`, app, documentFactory);
+
   const configService = app.get(ConfigService);
   const port = configService.get<string>("PORT", "3000");
 
   await app.listen(port, "0.0.0.0");
 
   const logger = app.get(Logger);
+  const actualUrl = `http://localhost:${port}`;
   logger.log(`App is ready and listening on port ${port} 🚀`);
+  logger.log(`Swagger UI available at ${actualUrl}/${GLOBAL_PREFIX}/doc 📄`);
 }
 
 bootstrap().catch(handleError);
